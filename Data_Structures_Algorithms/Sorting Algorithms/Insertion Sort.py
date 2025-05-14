36920eb0ae91433c2aff67497b643d6f917e9c4e
Shailesh <shaileshksingh4@gmail.com>
2025-05-14 10:12:01 +0530

    Insertion sorting explanation added

--- Data_Structures_Algorithms/Sorting Algorithms/Insertion Sort.py
+++ Data_Structures_Algorithms/Sorting Algorithms/Insertion Sort.py
@@ -1,21 +1,34 @@
-# Insertion Sort technique is used to list into two sections - sorted and unsorted.
+# Insertion Sort technique is used to split lists into two sections - sorted and unsorted.
 # Iterate through the unsorted section and insert every list item into their correct position in the sorted list.
-# This algorithm can also be implemented by using the ‘while’
+# This algorithm can also be implemented by using the ‘while’ loop
 
 def InsertionSort(array):
-    n = len(array)
+    n = len(array) # get the length of the input list array
 
+    # The outer loop starts from the second element (index 1) and goes through each element in the array.
     for i in range(1, n):
+
+        # keeps track of where this value should be inserted.
         insert_idx = i
 
+        # stores the element to be inserted into the sorted portion of the array
         current_value = array[i]
 
+        # This inner loop goes backward through the sorted portion of the array (from index i-1 to 0)
         for j in range(i - 1, -1, -1):
+
+            # If the current element in the sorted portion is greater than current_value,
             if array[j] > current_value:
+                # it is shifted one position to the right.
                 array[j + 1] = array[j]
+
+                # insert_idx is updated to the current index j, indicating where current_value should be inserted
                 insert_idx = j
             else:
+                # If the current element is not greater, the loop breaks early (since the rest of the array is already sorted).
                 break
+
+        # current_value is inserted at the correct position.
         array[insert_idx] = current_value
 
 # array to be sorted
